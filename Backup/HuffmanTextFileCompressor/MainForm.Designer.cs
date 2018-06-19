ff62a3c60a240f09ca89a77ca1fb6d76d8866cc0
Rezanina Lukas <john@doe.org>
2018-06-19 21:59:39 +0200

    skola

---
+++ Backup/HuffmanTextFileCompressor/MainForm.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace HuffmanTextFileCompressor
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.Filter = "Text Files (*.txt)|*.txt";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(13, 13);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "&Compress";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(503, 13);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "&Decompress";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(258, 13);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "C&lear";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Courier New", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(13, 43);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(565, 515);
+            this.textBox1.TabIndex = 3;
+            // 
+            // saveFileDialog1
+            // 
+            this.saveFileDialog1.Filter = "Huffman (*.huf)|*.huf";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(590, 570);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "MainForm - HuffmanTextFileCompression";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+    }
+}
+
---
+++ Backup/HuffmanTextFileCompressor/MainForm.cs
@@ -0,0 +1,437 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+
+namespace HuffmanTextFileCompressor
+{
+    public partial class MainForm : Form
+    {
+        private int iCount, leafNodes;
+        private List<CharFreq> charFreq;
+        public struct Header
+        {
+            public int inputFileLength;    // # ASCII input characters
+            public int tableLength;        // # of codes and chars
+            public int bitLength;          // # of bits per character
+            public int outputDataLength;   // # of output bytes
+            public byte[] chars;           // the characters in byte form
+            // probably should add hash value of input file but
+            // this is a public domain program so we can't export
+            // a cryptographically sound and seecure hash function
+        }
+        private Header header;
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        public int GetBit(int i, byte[] buffer)
+        {
+            int b = i % 8;
+            int c = i / 8;
+
+            return ((int)buffer[c] >> (7 - b)) & 1;
+        }
+
+        private void SetBit(int i, int v, byte[] buffer)
+        {
+            int b = i % 8;
+            int c = i / 8;
+            int mask = 1 << (7 - b);
+            int iBlock = (int)buffer[c];
+
+            if (v == 1)
+                iBlock |= mask;
+            else
+                iBlock &= ~mask;
+
+            buffer[c] = (byte)iBlock;
+        }
+
+        private void InorderTraversal(BinaryTreeNode<CharFreq> node)
+        {
+            if (node != null)
+            {
+                InorderTraversal(node.Left);
+
+                CharFreq cf = node.Value;
+                int ord = (int)cf.ch;
+
+                if (node.Left == null && node.Right == null)
+                {
+                    leafNodes++;
+                    textBox1.Text += ord.ToString("D3") + "\t";
+                    textBox1.Text += cf.freq.ToString() + "\r\n";
+                }
+
+                InorderTraversal(node.Right);
+            }
+        }
+
+        private void Compress()
+        {
+            int bitLength = 0, block = 0, bpBlock = 0;
+            HuffmanTree ht = new HuffmanTree();
+            BinaryTreeNode<CharFreq> root = ht.Build(charFreq, charFreq.Count);
+
+            leafNodes = 0;
+            textBox1.Text += "The Huffman tree inorder traversal\r\n\r\n";
+            InorderTraversal(root);
+
+            if (leafNodes != charFreq.Count)
+            {
+                MessageBox.Show("Compression tree build error", "Warning",
+                    MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            textBox1.Text += "\r\nTotal number of input characters = "
+                + iCount.ToString() + "\r\n";
+            textBox1.Text += "Total number of leaf characters = "
+                + leafNodes.ToString() + "\r\n";
+
+            if (leafNodes < 2)
+            {
+                bitLength = 1;
+                block = 8;
+                bpBlock = 1;
+            }
+            else if (leafNodes < 4)
+            {
+                bitLength = 2;
+                block = 4;
+                bpBlock = 1;
+            }
+            else if (leafNodes < 8)
+            {
+                bitLength = 3;
+                block = 8;
+                bpBlock = 3;
+            }
+            else if (leafNodes < 16)
+            {
+                bitLength = 4;
+                block = 2;
+                bpBlock = 1;
+            }
+            else if (leafNodes < 32)
+            {
+                bitLength = 5;
+                block = 8;
+                bpBlock = 5;
+            }
+            else if (leafNodes < 64)
+            {
+                bitLength = 6;
+                block = 4;
+                bpBlock = 3;
+            }
+            else if (leafNodes < 128)
+            {
+                bitLength = 7;
+                block = 8;
+                bpBlock = 8;
+            }
+
+            int number =  (leafNodes * bitLength) / block;
+            int numberBits = bitLength * iCount;
+            int numberBlocks = numberBits / (8 * bpBlock);
+            int remainder = numberBits % (8 * bpBlock);
+
+            header = new Header();
+            header.inputFileLength = iCount;
+            header.outputDataLength = numberBlocks * bpBlock + remainder / bitLength;
+            header.tableLength = leafNodes;
+            header.bitLength = bitLength;
+            header.chars = new byte[leafNodes];
+
+            textBox1.Text += "Total data length in bytes = "
+                + header.outputDataLength.ToString() + "\r\n";
+            textBox1.Text += "Total bit length = "
+                + bitLength.ToString() + "\r\n";
+
+            int headerLength = 4 * sizeof(int) + leafNodes * sizeof(byte);
+            int outputFileLength = headerLength + header.outputDataLength;
+            double percent = 100.0 - 100.0 * ((double)outputFileLength) / iCount;
+
+            textBox1.Text += "Total length of compressed file in bytes = "
+               + outputFileLength.ToString() + "\r\n";
+            textBox1.Text += "% Compression = "
+                + percent.ToString("F2") + "\r\n";
+
+            if (percent <= 0.0)
+            {
+                MessageBox.Show("% Compression is negative or zero", "Warning",
+                    MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            for (int i = 0; i < charFreq.Count; i++)
+                header.chars[i] = (byte)charFreq[i].ch;
+
+            try
+            {
+                if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+                {
+                    FileStream fs = new FileStream(saveFileDialog1.FileName,
+                        FileMode.Create);
+                    BinaryWriter bw = new BinaryWriter(fs);
+                    StreamReader sr = new StreamReader(openFileDialog1.FileName);
+                    byte[] ibuff = new byte[block];
+                    byte[] codes = new byte[block];
+
+                    bw.Write(header.inputFileLength);
+                    bw.Write(header.tableLength);
+                    bw.Write(header.bitLength);
+                    bw.Write(header.outputDataLength);
+                    bw.Write(header.chars, 0, header.tableLength);
+
+                    for (int k = 0; k < header.inputFileLength / block; k++)
+                    {
+                        for (int i = 0; i < block; i++)
+                        {
+                            ibuff[i] = (byte)sr.Read();
+                            codes[i] = (byte)255;
+
+                            for (int j = 0; j < header.tableLength; j++)
+                            {
+                                if (ibuff[i] == header.chars[j])
+                                {
+                                    codes[i] = (byte)j;
+                                    break;
+                                }
+                            }
+
+                            if (codes[i] == (byte)255)
+                            {
+                                MessageBox.Show("Code not found in lookup", "Warning",
+                                    MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                                return;
+                            }
+                        }
+
+                        byte[] obuff = new byte[bpBlock];
+
+                        for (int i = 0; i < block; i++)
+                        {
+                            int c = (int)codes[i];
+
+                            for (int j = 0; j < bitLength; j++)
+                            {
+                                int bit = (c >> (bitLength - j - 1)) & 1;
+
+                                SetBit(i * bitLength + j, bit, obuff);
+                            }
+                        }
+
+                        bw.Write(obuff, 0, bpBlock);
+                    }
+
+                    if (header.inputFileLength % block != 0)
+                    {
+                        for (int i = 0; i < header.inputFileLength % block; i++)
+                        {
+                            byte ch = (byte)sr.Read();
+
+                            bw.Write(ch);
+                        }
+                    }
+
+                    bw.Flush();
+                    sr.Close();
+                    bw.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString(), "Warning",
+                    MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                saveFileDialog1.Filter = "Huffman Files (*.huf)|*.huf";
+                openFileDialog1.Filter = "Text Files (*.txt)|*.txt";
+
+                if (openFileDialog1.ShowDialog() == DialogResult.OK)
+                {
+                    int[] freq = new int[128];
+                    StreamReader sr = new StreamReader(openFileDialog1.FileName);
+
+                    iCount = 0;
+
+                    while (!sr.EndOfStream)
+                    {
+                        int ch = sr.Read();
+
+                        if (ch >= 0 && ch <= 127)
+                            freq[ch]++;
+                        else
+                        {
+                            MessageBox.Show("File is not ASCII", "Warning",
+                                MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                            return;
+                        }
+
+                        iCount++;
+                    }
+
+                    sr.Close();
+                    charFreq = new List<CharFreq>(128);
+
+                    for (int i = 0; i < 128; i++)
+                    {
+                        if (freq[i] != 0)
+                        {
+                            CharFreq cf = new CharFreq();
+
+                            cf.ch = (char)i;
+                            cf.freq = freq[i];
+                            charFreq.Add(cf);
+                        }
+                    }
+
+                    Compress();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString(), "Warning",
+                    MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                openFileDialog1.Filter = "Huffman Files (*.huf)|*.huf";
+                saveFileDialog1.Filter = "Text Files (*.txt)|*.txt";
+
+                if (openFileDialog1.ShowDialog() == DialogResult.OK)
+                {
+                    if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+                    {
+                        FileStream fs = new FileStream(openFileDialog1.FileName,
+                            FileMode.Open);
+                        BinaryReader br = new BinaryReader(fs);
+                        StreamWriter sw = new StreamWriter(saveFileDialog1.FileName);
+                        int inputFileLength = br.ReadInt32(), block = 0, bpBlock = 0;
+                        int tableLength = br.ReadInt32(), totalWritten = 0;
+                        int bitLength = br.ReadInt32();
+                        int outputDataLength = br.ReadInt32();
+                        byte[] chars = br.ReadBytes(tableLength);
+
+                        textBox1.Text = string.Empty;
+                        textBox1.Text += "Input file length = "
+                            + inputFileLength.ToString() + "\r\n";
+                        textBox1.Text += "Table length = "
+                            + tableLength.ToString() + "\r\n";
+                        textBox1.Text += "Bit length = "
+                            + bitLength.ToString() + "\r\n";
+                        textBox1.Text += "Output data length = "
+                            + outputDataLength.ToString() + "\r\n";
+
+                        if (bitLength == 1)
+                        {
+                            block = 8;
+                            bpBlock = 1;
+                        }
+                        else if (bitLength == 2)
+                        {
+                            block = 4;
+                            bpBlock = 1;
+                        }
+                        else if (bitLength == 3)
+                        {
+                            block = 8;
+                            bpBlock = 3;
+                        }
+                        else if (bitLength == 4)
+                        {
+                            block = 2;
+                            bpBlock = 1;
+                        }
+                        else if (bitLength == 5)
+                        {
+                            block = 8;
+                            bpBlock = 5;
+                        }
+                        else if (bitLength == 6)
+                        {
+                            block = 4;
+                            bpBlock = 3;
+                        }
+                        else if (bitLength == 7)
+                        {
+                            block = 8;
+                            bpBlock = 8;
+                        }
+
+                        for (int i = 0; i < inputFileLength / block; i++)
+                        {
+                            byte[] ibuff = br.ReadBytes(bpBlock);
+
+                            for (int j = 0; j < block; j++)
+                            {
+                                int code = 0;
+
+                                for (int k = 0; k < bitLength; k++)
+                                {
+                                    int bit = GetBit(j * bitLength + k, ibuff);
+
+                                    code |= bit << (bitLength - k - 1);
+                                }
+
+                                char ch = (char)chars[code];
+
+                                sw.Write(ch);
+                                totalWritten++;
+                            }
+                        }
+
+                        if (inputFileLength % block != 0)
+                        {
+                            for (int i = 0; i < inputFileLength % block; i++)
+                            {
+                                byte ch = br.ReadByte();
+
+                                sw.Write(ch);
+                                totalWritten++;
+                            }
+                        }
+
+                        sw.Flush();
+                        br.Close();
+                        sw.Close();
+
+                        textBox1.Text += "Decompressed file length = "
+                            + totalWritten + "\r\n";
+
+                        if (totalWritten != inputFileLength)
+                        {
+                            MessageBox.Show("Houston we have a problem", "Warning",
+                                MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString(), "Warning",
+                    MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = string.Empty;
+        }
+    }
+}
